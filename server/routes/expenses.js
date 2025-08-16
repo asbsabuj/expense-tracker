17b3d4c348c6bc4a2f863b2431496a3bc1213167
Sabuj <asbsabuj1214@gmail.comgit config --global user.name Sabujgit commit -m 'first push'git config -global color.ui autogit config --global user.email asbsabuj1214@gmail.com>
2025-08-17 00:31:16 +0600

    seperated frontend and backend in sub directory

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -5,7 +5,21 @@ import { StatusCodes } from "http-status-codes"
 const router = express.Router()
 
 //add a new expense
-router.post("/", (req, res) => {})
+router.post("/", async (req, res) => {
+  const expenses = await new Expense({
+    title: req.body.title,
+    amount: req.body.amount,
+    date: req.body.date,
+    category: req.body.category,
+  })
+
+  try {
+    const newExpenses = await expenses.save()
+    res.status(StatusCodes.CREATED).json(newExpenses)
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: error.message })
+  }
+})
 
 //fetch all expense
 router.get("/", async (req, res) => {
